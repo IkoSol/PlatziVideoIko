9ee6b56855ed75ad93933b976fadce7174d48023
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-05-11 21:56:17 -0500

    ESLint. First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom' //Push all we do in React to the navigator
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), // detect the directory where the files are going to export and create the new folder named dist
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'] //Resolve extension we'll use on the proyect
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // identification of our js and jsx files
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html', //Where is the template we have
+            filename: './index.html' // the name that will have
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css' //This is the result file
+        })
+    ]
+}
