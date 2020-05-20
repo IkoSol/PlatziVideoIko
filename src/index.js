08b0503a3e694c25ec6ef2706bca421fc6d9d11c
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-05-19 22:14:01 -0500

    React Router added. Home added as first Route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom' //Push all we do in React to the navigator
-import App from './containers/App'
+import App from './routes/App'
 
 
 ReactDOM.render(<App />, document.getElementById('app'))
